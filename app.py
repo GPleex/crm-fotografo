cd7e8566d9ac08758c4753fee82373d18de7370f
Gustavo Alexander de Miranda <103151822+GPleex@users.noreply.github.com>
2025-08-29 15:46:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -607,4 +607,5 @@ def atualizar_campos_contrato(contrato, form):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
